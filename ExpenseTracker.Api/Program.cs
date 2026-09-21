a6bfcb7c449c204c7bf5140c43594f0ed96827f9
Yuliya <yuliya.petruk123@gmail.com>
2026-09-21 15:34:25 +0200

    added middleware and serilog (sink to file txt)

--- ExpenseTracker.Api/Program.cs
+++ ExpenseTracker.Api/Program.cs
@@ -1,8 +1,16 @@
-using Microsoft.EntityFrameworkCore;
 using ExpenseTracker.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,6 +21,15 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExpenseTracker.Api.Middleware.ExceptionHandlingMiddleware>();
+
+//esempio middleware 'light' in program.cs
+//app.Use(async (context, next) =>
+//{
+//    Console.WriteLine($"Richiesta in arrivo: {context.Request.Path}");
+//    await next(context); // passa al middleware successivo
+//    Console.WriteLine($"Risposta inviata: {context.Response.StatusCode}");
+//});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
